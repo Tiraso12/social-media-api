e3a4ede07069a9a3ca33f273617106f144797498
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-10-10 14:10:08 -0400

    creating controllers for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,6 +5,7 @@ const userController = {
     //get all users
     getAllUser({ }, res) {
         User.find({})
+        .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err =>{
             console.log(err);
@@ -17,6 +18,18 @@ const userController = {
         User.create(body)
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.json(err));
+    },
+
+    updateUser({ para, body}, res){
+        User.findOneAndUpdate({_id : params._id}, body, {new: true,})
+        .then(dbUserData =>{
+            if(!dbUserData){
+                res.status(404).json({message: 'No user found with this id!'})
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
     }
 };
 
